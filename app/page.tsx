a78ddf3eacc1f7ecb417b683f90601776b135f6b
Bisal-Shafiq <93635077+Bisal-Shafiq@users.noreply.github.com>
2024-11-29 18:42:16 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Home() {
+  return (
+    <>
+      
+      <main className="container mx-auto text-center p-8 m-20 ">
+        <h1 className="text-3xl font-bold text-center text-white">Welcome to MyApp!</h1>
+        <p className="mt-4 text-white">
+          This is a simple header with a responsive navbar and burger menu.
+        </p>
+      </main>
+    </>
+  );
+}
